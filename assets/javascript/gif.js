2b4f0cd1c68eb3e165e46b56ef6ca88ef56609ac
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2020-05-13 13:51:00 -0700

    Creating api key and connected to javascript using ajax

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,11 +1,91 @@
 $(document).ready(function () {
 
+// Initial array of movies
+var charactors = ["Iron Man", "Captain America", "Black Widow", "Thor","Hulk"];
+// displayMovieInfo function re-renders the HTML to display the appropriate content
+function displayCharactorInfo() {
+
+ var charactor = $(this).attr("data-search");
+ var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +charactor+ "&api_key=6Udiqq9v5j02FwXO6oJOOWE4kA2EA6Q2&limit=10";
+
+  // Creating an AJAX call for the specific movie button being clicked
+  $.ajax({
+      url: queryURL,
+      method: "GET"
+  }).then(function(response) {
+
+// Storing an array of results in the results variable
+var results = response.data;
+
+// Looping over every result item
+for (var i = 0; i < results.length; i++) {
+ 
+ // Only taking action if the photo has an appropriate rating
+ if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+ 
+ 
+// Creating a div to hold the charactor
+var charactorDiv = $("<div class = 'charactor'>");
+
+// Storing the rating data
+var rating = results[i].rating;
+
+// Creating an element to have the rating displayed
+var pRating = $("<p>").txet("Rating :  " + rating);
+
+// Creating an image tag
+ var imgURL = $("<img>") ;
+
+ // Giving the image tag an src attribute of a proprty pulled off the
+// result item
+imgURL.attr("src", results[i].images.fixed_height.url);
+
+// Appending the paragraph and imgURL we created to the "charactorDiv" div we created
+//To Display images and rating
+charactorDiv.append(imgURL);
+charactorDiv.append(pRating);
+
+// Prepending the charactorDiv to the "#gifs-appear-here" div in the HTML
+$("#gifs-appear-here").prepend(charactorDiv);
+
+       }
+
+   };
+
+ });
+
+};
+
+// Function for displaing charactor data
+
+function renderButtons(){
+
+// Deleting the charactors prior to adding new charactors
+// (this is necessary otherwise you will have repeat buttons)
+$("#btn-view").empty();
+
+// Looping through the array of charactors
+for (var i = 0; i < charactors.length; i++){
+
+// Then dynamicaly generating buttons for each charactor in the array
+// This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+    var a = $("<button");
+//Adding a class of charactor-btn to our button 
+        a.addClass("charactor-btn");
+// Adding a data-attribute
+        a.attr("data-search", charactors[i]);
+// Providing the initial button text
+        a.text(charactor[i]);
+// Adding the button to the btn-view div
+        $("btn-view").append(a);
+
+}
 
 
 
 
 
-
+};
 
 
 
